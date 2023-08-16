327b91f11119b1cc16713c6b087a61baa8584d46
Tory <88531407+1two13@users.noreply.github.com>
2023-08-16 12:38:34 +0900

    feat: Day2 다른 코드 추가

--- Week1/Day2/Solution.js
+++ Week1/Day2/Solution.js
@@ -29,3 +29,37 @@ rl.on('close', () => {
 
   rl.close();
 });
+
+//! 다른 코드
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+
+rl.on('line', (line) => {
+  input.push(line.trim());
+  // 마지막으로 입력된 데이터에 줄바꿈이 없다면 마지막 데이터가 들어오지 않기 때문에(해당 문제에서는 해당되지 않음)
+  // 확실하게 하기 위해 조건식 설정
+  if (input.length === Number(input[0]) + 2) {
+    rl.close();
+  }
+});
+
+rl.on('close', () => {
+  let N = Number(input[0]);
+  let [currentHour, currentMinute] = input[1].split(' ').map(Number);
+
+  for (let i = 0; i < N; i++) {
+    let costMinute = Number(input[i + 2]);
+    let tempMinute = currentMinute + costMinute;
+    let resultMinute = tempMinute % 60;
+    let resultHour = (currentHour + Math.floor(tempMinute / 60)) % 24;
+
+    currentHour = resultHour;
+    currentMinute = resultMinute;
+  }
+
+  console.log(currentHour, currentMinute);
+});
