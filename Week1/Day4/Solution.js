800bd52ecc8b83cd64f7ee016c0dc671d0bb82a0
Tory <88531407+1two13@users.noreply.github.com>
2023-08-17 20:02:37 +0900

    refactor: Day4

--- Week1/Day4/Solution.js
+++ Week1/Day4/Solution.js
@@ -3,7 +3,6 @@
 // 출력값: 입력값을 모두 더한 값
 // 완벽하지 않은 출력값: return 0
 
-//! 하나 fail
 const readline = require('readline');
 let rl = readline.createInterface({
   input: process.stdin,
@@ -21,21 +20,22 @@ rl.on('close', () => {
   const maxIndex = arr.indexOf(max);
   const before = arr.slice(0, maxIndex);
   const after = arr.slice(maxIndex + 1);
-  let flag = false;
+  let flag = true;
 
   for (let i = 0; i < before.length - 1; i++) {
-    if (before[i] <= before[i + 1]) flag = true;
-    else {
+    if (before[i] > before[i + 1]) {
       flag = false;
       break;
     }
   }
 
-  for (let i = 0; i < after.length - 1; i++) {
-    if (after[i] >= after[i + 1]) flag = true;
-    else {
-      flag = false;
-      break;
+  // flag가 true일 때만 after 값 비교
+  if (flag) {
+    for (let i = 0; i < after.length - 1; i++) {
+      if (after[i] < after[i + 1]) {
+        flag = false;
+        break;
+      }
     }
   }
 
