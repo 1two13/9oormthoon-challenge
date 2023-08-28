0788cfc639d1fbf581cefb6a52ad5aa8eb51fcab
Tory <88531407+1two13@users.noreply.github.com>
2023-08-28 16:31:07 +0900

    feat: 코드 추가

--- Week2/Day4/Solution.js
+++ Week2/Day4/Solution.js
@@ -1,3 +1,49 @@
+const readline = require('readline');
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+
+rl.on('line', (line) => {
+  input.push(line);
+});
+
+rl.on('close', () => {
+  let n = Number(input[0].split(' ')[0]);
+  let result = Array.from({ length: n }, () => Array(n).fill(0));
+  const board = input.slice(1, n + 1).map((el) => el.split(' '));
+  const indexList = input.slice(n + 1).map((el) => el.split(' ').map((el) => Number(el) - 1));
+
+  for (let i = 0; i < indexList.length; i++) {
+    // indexList 순회
+    let [x, y] = indexList[i];
+
+    // 본인 좌표
+    if (board[x][y] === '0') result[x][y] += 1;
+    else if (board[x][y] === '@') result[x][y] += 2;
+
+    // 좌표 기준으로 상하좌우로 폭탄이 터짐 => result에 기록
+    for (let j = 0; j < 4; j++) {
+      let newX = x + dx[j];
+      let newY = y + dy[j];
+
+      // board 범위 안에 있고,
+      if (newX >= 0 && newY >= 0 && newX < n && newY < n && board[newX][newY] !== '#') {
+        if (board[newX][newY] === '@') result[newX][newY] += 2;
+        else result[newX][newY] += 1;
+      }
+    }
+  }
+
+  console.log(Math.max(...result.flat()));
+
+  rl.close();
+});
+
 //! 다른 코드
 const readline = require('readline');
 const rl = readline.createInterface({
